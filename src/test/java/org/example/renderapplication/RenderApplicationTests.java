3a7a969b360e0e3d944200814971e07a9d2adcd0
Gustavo De Longhi <gustavodelonghi@gmail.com>
2025-08-29 16:41:21 -0300

    initial commit

---
+++ src/test/java/org/example/renderapplication/RenderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.renderapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
